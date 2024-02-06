4dc26148a30f5698b6c8464d4c16d69c85494d57
sssh199x <sandeshhamal5890@gmail.com>
2024-02-06 16:11:45 +0545

    Add: notification_services

--- lib/notification_services.dart
+++ lib/notification_services.dart
@@ -1,9 +1,14 @@
 import 'package:firebase_messaging/firebase_messaging.dart';
+import 'package:flutter/material.dart';
+import 'package:app_settings/app_settings.dart';
 
 class NotificationServices {
+  // To get a new instance of the FirebaseMessaging
   FirebaseMessaging messaging = FirebaseMessaging.instance;
 
-  void requestNotificationPermission() async {
+  void requestNotificationPermission(BuildContext context) async {
+    // Prompts the user for notification permissions.
+    // Note that on iOS, if [provisional] is set to true, silent notification permissions will be automatically granted. When notifications are delivered to the device, the user will be presented with an option to disable notifications, keep receiving them silently or enable prominent notifications.
     NotificationSettings settings = await messaging.requestPermission(
         alert: true,
         announcement: true,
@@ -12,5 +17,52 @@ class NotificationServices {
         criticalAlert: true,
         provisional: true,
         sound: true);
+
+    if (settings.authorizationStatus == AuthorizationStatus.authorized) {
+      print('user acces granted');
+    } else if (settings.authorizationStatus ==
+        AuthorizationStatus.provisional) {
+      print('user granted provisional permission');
+    } else if (settings.authorizationStatus ==
+        AuthorizationStatus.notDetermined) {
+      print('user permission not determined');
+    } else {
+      print('user permission denied');
+      _showSettingsDialog(context);
+    }
+  }
+
+  // Show dialog to guide the user to open settings
+  void _showSettingsDialog(BuildContext context) {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: const Text("Notification Permission Denied"),
+          content: const Text(
+              "To enable notifications, please go to Settings and allow notifications for this app."),
+          actions: <Widget>[
+            TextButton(
+              onPressed: () {
+                _openAppSettings();
+                Navigator.of(context).pop();
+              },
+              child: const Text("Open Settings"),
+            ),
+            TextButton(
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+              child: const Text("Cancel"),
+            ),
+          ],
+        );
+      },
+    );
+  }
+
+  // Open app settings
+  void _openAppSettings() {
+    AppSettings.openAppSettings(type: AppSettingsType.notification);
   }
 }
