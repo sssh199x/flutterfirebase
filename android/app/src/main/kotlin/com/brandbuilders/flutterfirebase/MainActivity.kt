20a2ba185f06a7ee5d64699c31cd2476b23e2416
sssh199x <sandeshhamal5890@gmail.com>
2024-02-06 12:14:58 +0545

    first commit

---
+++ android/app/src/main/kotlin/com/brandbuilders/flutterfirebase/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.brandbuilders.flutterfirebase
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/notification_services.dart
@@ -0,0 +1,16 @@
+import 'package:firebase_messaging/firebase_messaging.dart';
+
+class NotificationServices {
+  FirebaseMessaging messaging = FirebaseMessaging.instance;
+
+  void requestNotificationPermission() async {
+    NotificationSettings settings = await messaging.requestPermission(
+        alert: true,
+        announcement: true,
+        badge: true,
+        carPlay: true,
+        criticalAlert: true,
+        provisional: true,
+        sound: true);
+  }
+}
