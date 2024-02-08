58ed0953efe8ef846d29c8909f162f6d47b5d84e
sssh199x <sandeshhamal5890@gmail.com>
2024-02-08 16:51:11 +0545

    Add :New lines of code

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,24 @@
+import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutterfirebase/home_screen.dart';
 import 'firebase_options.dart';
+
+// This should always be in the top level of your projec( so its called in main function)t otherwise we can get different errors such as redirection issues.This function is used to handle the background notifications for your app
+// And this should also be the entry point of your code , we can make it the entry point of our app by writing @pragma('vm:entry-point').If this is not done then we wont get the notifications when our app is in terminated state.
+
+@pragma('vm-entry-point')
+Future<void> _firebaseMessagingBackgroundHandler(RemoteMessage message) async {
+  await Firebase.initializeApp();
+  print(message.notification!.title.toString());
+}
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
+  FirebaseMessaging.onBackgroundMessage(_firebaseMessagingBackgroundHandler);
   runApp(const MyApp());
 }
 
