e9cc031e1c90c95a208ad2d70d1bea15649f25f5
sssh199x <sandeshhamal5890@gmail.com>
2024-02-11 15:28:37 +0545

    Add: Initialize foregroundMessageForIos

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -1,5 +1,8 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
 import 'package:flutterfirebase/notification_services.dart';
+import 'package:http/http.dart' as http;
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -15,6 +18,7 @@ class _HomeScreenState extends State<HomeScreen> {
     // TODO: implement initState
     super.initState();
     notificationServices.requestNotificationPermission(context);
+    notificationServices.foregroundMessageForIos();
     notificationServices.firebaseInit(context);
     notificationServices.setupInteractedMessage(context);
     notificationServices.isTokenRefresh();
@@ -31,6 +35,43 @@ class _HomeScreenState extends State<HomeScreen> {
         backgroundColor: Colors.blue,
         title: const Text('Flutter Notifications'),
       ),
+      body: Center(
+          child: TextButton(
+              style: ButtonStyle(
+                  backgroundColor: MaterialStateProperty.all(Colors.black12)),
+              onPressed: () {
+                notificationServices.getDeviceToken().then((value) async {
+                  var data = {
+                    'priority': 'high',
+                    'to': value,
+                    'notification': {
+                      'body': 'Are you lost?',
+                      'title': 'Hey! Baby Girl?'
+                    },
+                    // payload data
+                    'data': {
+                      // extra data defined by myself
+                      'click_action': 'FLUTTER_NOTIFICATION_CLICK',
+
+                      'type': 'message',
+                      'id': '1234',
+                      // extra data defined by myself
+                      'status': 'done'
+                    }
+                  };
+                  await http.post(
+                    Uri.parse('https://fcm.googleapis.com/fcm/send'),
+                    body: jsonEncode(data),
+                    headers: {
+                      'Content-Type': 'application/json; charset=UTF-8',
+                      // This is the server key from firebase console
+                      'Authorization':
+                          'key=AAAAUtXr9k4:APA91bGa1c10c4QqAGKdKx1uw7gYCpk3qtR4AeyBO0aaCut7PP9FMHHPEjPp0yuR2L1LXxhA7hsT5cxBtpcHMdaxXhDsGtuZBbJbnxrkruqM-8gss8v2l-Z95A5VeGj4pzM22rww558f'
+                    },
+                  );
+                });
+              },
+              child: const Text('Send Notifications'))),
     );
   }
 }
